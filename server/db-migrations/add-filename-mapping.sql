5bb791622476642a9040778e43b3233573a83848
moassem1978 <moassem1978@gmail.com>
2025-07-11 12:48:51 +0300

    Delete server directory

--- server/db-migrations/add-filename-mapping.sql
+++
@@ -1,9 +0,0 @@
-
--- Add filename mapping support to image_backups table
-ALTER TABLE image_backups ADD COLUMN IF NOT EXISTS filename_map TEXT;
-
--- Create index for faster filename lookups
-CREATE INDEX IF NOT EXISTS idx_image_backups_filename_map ON image_backups USING gin((filename_map::jsonb));
-
--- Update existing backups to include filename mapping where possible
-UPDATE image_backups SET filename_map = '{}' WHERE filename_map IS NULL;
